e1cec4e5d43c028c63fb3cbfb48e076ab26eff67
Evgeniy <evgeniy.bronnikov@ovcenter.com>
2019-10-04 16:50:33 +0300

    genres

---
+++ src/Section/Ganres/MovieList.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace TMDB\Section\Ganres;
+
+use TMDB\Section\AbstractSection;
+
+/**
+ * Class MovieList
+ * @package TMDB\Section\Ganres
+ */
+class MovieList extends AbstractSection
+{
+    protected $path = '/genre/movie/list';
+}
---
+++ src/Section/Ganres/TvList.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace TMDB\Section\Ganres;
+
+use TMDB\Section\AbstractSection;
+
+/**
+ * Class TvList
+ * @package TMDB\Section\Ganres
+ */
+class TvList extends AbstractSection
+{
+    protected $path = '/genre/tv/list';
+}
