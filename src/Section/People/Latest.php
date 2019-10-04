2570eb209564c870cb95eef859218191a33e8420
Evgeniy <evgeniy.bronnikov@ovcenter.com>
2019-10-04 16:54:15 +0300

    people: latest; popular

---
+++ src/Section/People/Latest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace TMDB\Section\People;
+
+use TMDB\Section\AbstractSection;
+
+/**
+ * Class Latest
+ * @package TMDB\Section\People
+ */
+class Latest extends AbstractSection
+{
+    protected $path = '/person/latest';
+}
---
+++ src/Section/People/Popular.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace TMDB\Section\People;
+
+use TMDB\Section\AbstractSection;
+
+/**
+ * Class Popular
+ * @package TMDB\Section\People
+ */
+class Popular extends AbstractSection
+{
+    protected $path = '/person/popular';
+}
